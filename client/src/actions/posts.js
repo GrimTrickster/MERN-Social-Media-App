7bfd6a6d2a75cbf8a383bb95840972400e87cb0b
grim.trickster04@gmail.com <grim.trickster04@gmail.com>
2022-09-03 21:25:20 +0200

    Implemented Details

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,7 +1,20 @@
-import { FETCH_ALL, CREATE, DELETE, UPDATE, FETCH_BY_SEARCH, START_LOADING, END_LOADING, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, CREATE, DELETE, UPDATE, FETCH_BY_SEARCH, START_LOADING, END_LOADING, LIKE, FETCH_POST } from '../constants/actionTypes';
 import * as api from '../api';
 
 // Action Creators
+export const getPost = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: START_LOADING });
+        const { data } = await api.fetchPost(id);
+
+        dispatch({ type: FETCH_POST, payload: data });
+        dispatch({ type: END_LOADING });
+
+    } catch (error) {
+        console.log(error.message)
+    }
+};
+
 export const getPosts = (page) => async (dispatch) => {
 
     try {
@@ -14,7 +27,7 @@ export const getPosts = (page) => async (dispatch) => {
     } catch (error) {
         console.log(error.message)
     }
-}
+};
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
